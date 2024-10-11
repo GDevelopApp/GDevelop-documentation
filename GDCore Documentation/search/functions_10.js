ea86ae83062d94034ff2d9836434ae2705347fb5
GDevelop documentation CI <ci-builder@gdevelop.io>
2024-10-11 13:30:30 +0000

    Update documentation [skip ci]

--- GDCore Documentation/search/functions_10.js
+++ GDCore Documentation/search/functions_10.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['to_3854',['To',['../classgd_1_1_string.html#ae7dd2330f73d88e5af2ae2d7b0af81a3',1,'gd::String']]],
-  ['toescapedxmlstring_3855',['ToEscapedXMLString',['../classgd_1_1_serializer.html#a0560287d898cf2e308b3ea675e6ab829',1,'gd::Serializer']]],
-  ['tojson_3856',['ToJSON',['../classgd_1_1_serializer.html#a42096ae46304049ee773a5260d5e1ef0',1,'gd::Serializer']]],
-  ['tolocale_3857',['ToLocale',['../classgd_1_1_string.html#a38361bb24ca3cd24c1f44241f8801999',1,'gd::String']]],
-  ['toutf32_3858',['ToUTF32',['../classgd_1_1_string.html#a410d7030da3bdd6061b0acd064d9d1a4',1,'gd::String']]],
-  ['toutf8_3859',['ToUTF8',['../classgd_1_1_string.html#a4348a307a86bdd733f0cb1941acaff67',1,'gd::String']]],
-  ['towide_3860',['ToWide',['../classgd_1_1_string.html#ae588f705053728b1a776486b54f441bc',1,'gd::String']]],
-  ['transcode_3861',['Transcode',['../struct_transcoder.html#a0ea2edfe35784ebf1063921d2bd5fb66',1,'Transcoder']]],
-  ['trim_3862',['Trim',['../classgd_1_1_string.html#aadb036ecf85f0cf420e603c5d168c950',1,'gd::String']]]
+  ['to_3855',['To',['../classgd_1_1_string.html#ae7dd2330f73d88e5af2ae2d7b0af81a3',1,'gd::String']]],
+  ['toescapedxmlstring_3856',['ToEscapedXMLString',['../classgd_1_1_serializer.html#a0560287d898cf2e308b3ea675e6ab829',1,'gd::Serializer']]],
+  ['tojson_3857',['ToJSON',['../classgd_1_1_serializer.html#a42096ae46304049ee773a5260d5e1ef0',1,'gd::Serializer']]],
+  ['tolocale_3858',['ToLocale',['../classgd_1_1_string.html#a38361bb24ca3cd24c1f44241f8801999',1,'gd::String']]],
+  ['toutf32_3859',['ToUTF32',['../classgd_1_1_string.html#a410d7030da3bdd6061b0acd064d9d1a4',1,'gd::String']]],
+  ['toutf8_3860',['ToUTF8',['../classgd_1_1_string.html#a4348a307a86bdd733f0cb1941acaff67',1,'gd::String']]],
+  ['towide_3861',['ToWide',['../classgd_1_1_string.html#ae588f705053728b1a776486b54f441bc',1,'gd::String']]],
+  ['transcode_3862',['Transcode',['../struct_transcoder.html#a0ea2edfe35784ebf1063921d2bd5fb66',1,'Transcoder']]],
+  ['trim_3863',['Trim',['../classgd_1_1_string.html#aadb036ecf85f0cf420e603c5d168c950',1,'gd::String']]]
 ];
--- GDCore Documentation/search/pages_1.js
+++ GDCore Documentation/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['gdevelop_20core_4228',['GDevelop Core',['../index.html',1,'']]]
+  ['gdevelop_20core_4229',['GDevelop Core',['../index.html',1,'']]]
 ];
