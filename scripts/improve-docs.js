3ba43f41e909df0f6cd62e10cc33cb1e61abc5e1
Florian Rival <Florian.rival@gmail.com>
2026-02-18 12:53:22 +0100

    Improve automated doc writing instructions (2)

--- scripts/improve-docs.js
+++ scripts/improve-docs.js
@@ -176,8 +176,17 @@ IMPORTANT CONSTRAINTS
 • NEVER create new files unless absolutely necessary.
 • NEVER edit files outside docs/gdevelop5/ (except automated_updates_data.json as described below).
 • Do NOT touch images or binary files.
+
+IMPORTANT WRITING STYLE AND ARCHITECTURE CONSTRAINTS
+----------------------------------------------------
+
 • Do NOT document non-user-facing implementation details. Focus only on information
   that is relevant to users creating games with GDevelop, not internal engine behavior.
+• Do NOT list all actions/conditions in a list or table: the reference documentations are already
+  automatically generated for this.
+• Do not specify the default values. Don't feel forced to list all the properties of a 
+  behavior/object/condition/action: prefer to explain use cases, examples and how it's useful
+  for the end user which will create a game with it.
 
 WHEN YOU ARE DONE
 -----------------
