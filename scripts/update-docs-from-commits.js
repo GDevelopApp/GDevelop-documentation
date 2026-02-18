d61a80fa225dbdded1abfedf03b9017c2db234ef
Florian Rival <Florian.rival@gmail.com>
2026-02-18 12:53:14 +0100

    Improve automated doc writing instructions

--- scripts/update-docs-from-commits.js
+++ scripts/update-docs-from-commits.js
@@ -209,8 +209,18 @@ IMPORTANT CONSTRAINTS
 • NEVER create new files unless a major new feature truly requires it.
 • NEVER edit files outside docs/gdevelop5/.
 • Do NOT touch images or binary files.
-• Do not specify the default values. Don't feel forced to list all the properties of a behavior/object/condition/action: prefer to explain use cases, examples and how it's useful for the end user which will create a game with it.
 • If no documentation updates are needed, do nothing.
+
+IMPORTANT WRITING STYLE AND ARCHITECTURE CONSTRAINTS
+----------------------------------------------------
+
+• Do NOT document non-user-facing implementation details. Focus only on information
+  that is relevant to users creating games with GDevelop, not internal engine behavior.
+• Do NOT list all actions/conditions in a list or table: the reference documentations are already
+  automatically generated for this.
+• Do not specify the default values. Don't feel forced to list all the properties of a 
+  behavior/object/condition/action: prefer to explain use cases, examples and how it's useful
+  for the end user which will create a game with it.
 
 WHEN YOU ARE DONE
 -----------------
