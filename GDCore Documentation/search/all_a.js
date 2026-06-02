ccec6ffed37a5fcd666b7d88663ee4634ddae08c
GDevelop documentation CI <ci-builder@gdevelop.io>
2026-06-02 00:14:53 +0000

    Update documentation [skip ci]

--- GDCore Documentation/search/all_a.js
+++ GDCore Documentation/search/all_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['javascriptresource_1273',['JavaScriptResource',['../classgd_1_1_java_script_resource.html',1,'gd']]],
-  ['jsonresource_1274',['JsonResource',['../classgd_1_1_json_resource.html',1,'gd']]]
+  ['javascriptresource_1272',['JavaScriptResource',['../classgd_1_1_java_script_resource.html',1,'gd']]],
+  ['jsonresource_1273',['JsonResource',['../classgd_1_1_json_resource.html',1,'gd']]]
 ];
--- GDCore Documentation/search/pages_1.js
+++ GDCore Documentation/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['gdevelop_20core_4567',['GDevelop Core',['../index.html',1,'']]]
+  ['gdevelop_20core_4566',['GDevelop Core',['../index.html',1,'']]]
 ];
