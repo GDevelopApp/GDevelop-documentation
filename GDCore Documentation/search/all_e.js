1ce5cadd7fadab26a6662415d9741478f6776a52
GDevelop documentation CI <ci-builder@gdevelop.io>
2026-09-11 00:12:47 +0000

    Update documentation [skip ci]

--- GDCore Documentation/search/all_e.js
+++ GDCore Documentation/search/all_e.js
@@ -1,18 +1,19 @@
 var searchData=
 [
-  ['name_1427',['name',['../struct_generic_pointer_1_1_token.html#a8aa9b13bd66addb0c0512cfcae72174c',1,'GenericPointer::Token::name()'],['../classgd_1_1_object.html#a6b913c183be7fe5a65de77ac21543d3f',1,'gd::Object::name()'],['../struct_generic_member.html#a7124f7ccd67421533d33139938604fac',1,'GenericMember::name()']]],
+  ['name_1427',['name',['../classgd_1_1_object.html#a6b913c183be7fe5a65de77ac21543d3f',1,'gd::Object::name()'],['../struct_generic_member.html#a7124f7ccd67421533d33139938604fac',1,'GenericMember::name()'],['../struct_generic_pointer_1_1_token.html#a8aa9b13bd66addb0c0512cfcae72174c',1,'GenericPointer::Token::name()']]],
   ['namedpropertydescriptor_1428',['NamedPropertyDescriptor',['../classgd_1_1_named_property_descriptor.html',1,'gd::NamedPropertyDescriptor'],['../classgd_1_1_named_property_descriptor.html#a658289e1d7ed8773affb48e846b9ba1f',1,'gd::NamedPropertyDescriptor::NamedPropertyDescriptor()']]],
-  ['newfilenames_1429',['newFilenames',['../classgd_1_1_resources_merging_helper.html#a94c8fc250d9489295df5f309a6aa93de',1,'gd::ResourcesMergingHelper']]],
-  ['newnamegenerator_1430',['NewNameGenerator',['../classgd_1_1_new_name_generator.html',1,'gd']]],
-  ['nfc_1431',['NFC',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617adcd3a91f13022012f23a7f761c926309',1,'gd::String']]],
-  ['nfd_1432',['NFD',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a34c0f9c8b54e306ae100f3e289f8216b',1,'gd::String']]],
-  ['nfkc_1433',['NFKC',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a5c1d833ab57bc4a55b6f631b1c684436',1,'gd::String']]],
-  ['nfkd_1434',['NFKD',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a71a8daec0f8e80209e36b967b2a25214',1,'gd::String']]],
-  ['nonconstiterator_1435',['NonConstIterator',['../class_generic_member_iterator.html#abc26eb06f2962765b11dcd06ce84ac02',1,'GenericMemberIterator']]],
-  ['normalize_1436',['Normalize',['../classgd_1_1_string.html#a4e4659edb27640882458570e746b4bff',1,'gd::String']]],
-  ['normalizeseparator_1437',['NormalizeSeparator',['../classgd_1_1_abstract_file_system.html#a87424272a61c3ad3e1a6212c248fd2df',1,'gd::AbstractFileSystem']]],
-  ['normform_1438',['NormForm',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617',1,'gd::String']]],
-  ['not_5fenough_5froom_1439',['not_enough_room',['../classutf8_1_1not__enough__room.html',1,'utf8']]],
-  ['number_1440',['Number',['../union_generic_value_1_1_number.html',1,'GenericValue']]],
-  ['numbernode_1441',['NumberNode',['../structgd_1_1_number_node.html',1,'gd']]]
+  ['namegeneration_1429',['nameGeneration',['../classgd_1_1_object.html#a09f98ee7dc780d01e823e7eff319a602',1,'gd::Object']]],
+  ['newfilenames_1430',['newFilenames',['../classgd_1_1_resources_merging_helper.html#a94c8fc250d9489295df5f309a6aa93de',1,'gd::ResourcesMergingHelper']]],
+  ['newnamegenerator_1431',['NewNameGenerator',['../classgd_1_1_new_name_generator.html',1,'gd']]],
+  ['nfc_1432',['NFC',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617adcd3a91f13022012f23a7f761c926309',1,'gd::String']]],
+  ['nfd_1433',['NFD',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a34c0f9c8b54e306ae100f3e289f8216b',1,'gd::String']]],
+  ['nfkc_1434',['NFKC',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a5c1d833ab57bc4a55b6f631b1c684436',1,'gd::String']]],
+  ['nfkd_1435',['NFKD',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617a71a8daec0f8e80209e36b967b2a25214',1,'gd::String']]],
+  ['nonconstiterator_1436',['NonConstIterator',['../class_generic_member_iterator.html#abc26eb06f2962765b11dcd06ce84ac02',1,'GenericMemberIterator']]],
+  ['normalize_1437',['Normalize',['../classgd_1_1_string.html#a4e4659edb27640882458570e746b4bff',1,'gd::String']]],
+  ['normalizeseparator_1438',['NormalizeSeparator',['../classgd_1_1_abstract_file_system.html#a87424272a61c3ad3e1a6212c248fd2df',1,'gd::AbstractFileSystem']]],
+  ['normform_1439',['NormForm',['../classgd_1_1_string.html#ac9fbe9e33c5ebfa40ef54ff848702617',1,'gd::String']]],
+  ['not_5fenough_5froom_1440',['not_enough_room',['../classutf8_1_1not__enough__room.html',1,'utf8']]],
+  ['number_1441',['Number',['../union_generic_value_1_1_number.html',1,'GenericValue']]],
+  ['numbernode_1442',['NumberNode',['../structgd_1_1_number_node.html',1,'gd']]]
 ];
--- GDCore Documentation/search/variables_c.js
+++ GDCore Documentation/search/variables_c.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['objectfunctionorbehaviorname_4393',['objectFunctionOrBehaviorName',['../structgd_1_1_object_function_name_node.html#a51430c971008d261ecf323750b71a61e',1,'gd::ObjectFunctionNameNode']]],
-  ['objectfunctionorbehaviornamelocation_4394',['objectFunctionOrBehaviorNameLocation',['../structgd_1_1_object_function_name_node.html#adb286abe854db63b9291f3924ff7bbcb',1,'gd::ObjectFunctionNameNode']]],
-  ['objectnamedotlocation_4395',['objectNameDotLocation',['../structgd_1_1_object_function_name_node.html#ac7e686230459fbe1502dbec8b7468912',1,'gd::ObjectFunctionNameNode::objectNameDotLocation()'],['../structgd_1_1_function_call_node.html#a575b08814ecb4ea898b9a4362dab9f47',1,'gd::FunctionCallNode::objectNameDotLocation()']]],
-  ['objectnamelocation_4396',['objectNameLocation',['../structgd_1_1_object_function_name_node.html#a5e983fd13f99c8980878c36a18a7660b',1,'gd::ObjectFunctionNameNode::objectNameLocation()'],['../structgd_1_1_function_call_node.html#a33c48b50c47ce4e25a75ac87669c9f35',1,'gd::FunctionCallNode::objectNameLocation()']]],
-  ['objectvariables_4397',['objectVariables',['../classgd_1_1_object.html#a44f98d0805ceead0c797f6b582782382',1,'gd::Object']]],
-  ['oldfilenames_4398',['oldFilenames',['../classgd_1_1_resources_merging_helper.html#ad592e6a0cb0716ece5b605552d86005a',1,'gd::ResourcesMergingHelper']]],
-  ['openingparenthesislocation_4399',['openingParenthesisLocation',['../structgd_1_1_function_call_node.html#a68fa955ce20704b84c1ce7ddffe25d16',1,'gd::FunctionCallNode']]],
-  ['originalevent_4400',['originalEvent',['../classgd_1_1_base_event.html#aaf900d47cf54bd3efee28dcba68911b9',1,'gd::BaseEvent']]]
+  ['objectfunctionorbehaviorname_4395',['objectFunctionOrBehaviorName',['../structgd_1_1_object_function_name_node.html#a51430c971008d261ecf323750b71a61e',1,'gd::ObjectFunctionNameNode']]],
+  ['objectfunctionorbehaviornamelocation_4396',['objectFunctionOrBehaviorNameLocation',['../structgd_1_1_object_function_name_node.html#adb286abe854db63b9291f3924ff7bbcb',1,'gd::ObjectFunctionNameNode']]],
+  ['objectnamedotlocation_4397',['objectNameDotLocation',['../structgd_1_1_object_function_name_node.html#ac7e686230459fbe1502dbec8b7468912',1,'gd::ObjectFunctionNameNode::objectNameDotLocation()'],['../structgd_1_1_function_call_node.html#a575b08814ecb4ea898b9a4362dab9f47',1,'gd::FunctionCallNode::objectNameDotLocation()']]],
+  ['objectnamelocation_4398',['objectNameLocation',['../structgd_1_1_object_function_name_node.html#a5e983fd13f99c8980878c36a18a7660b',1,'gd::ObjectFunctionNameNode::objectNameLocation()'],['../structgd_1_1_function_call_node.html#a33c48b50c47ce4e25a75ac87669c9f35',1,'gd::FunctionCallNode::objectNameLocation()']]],
+  ['objectvariables_4399',['objectVariables',['../classgd_1_1_object.html#a44f98d0805ceead0c797f6b582782382',1,'gd::Object']]],
+  ['oldfilenames_4400',['oldFilenames',['../classgd_1_1_resources_merging_helper.html#ad592e6a0cb0716ece5b605552d86005a',1,'gd::ResourcesMergingHelper']]],
+  ['openingparenthesislocation_4401',['openingParenthesisLocation',['../structgd_1_1_function_call_node.html#a68fa955ce20704b84c1ce7ddffe25d16',1,'gd::FunctionCallNode']]],
+  ['originalevent_4402',['originalEvent',['../classgd_1_1_base_event.html#aaf900d47cf54bd3efee28dcba68911b9',1,'gd::BaseEvent']]]
 ];
